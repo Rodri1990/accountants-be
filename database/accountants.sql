e2d2788e074d511fca18a6765c1855a9a45e21d8
Rodri1990 <105568525+Rodri1990@users.noreply.github.com>
2022-06-03 23:11:04 -0300

    Update accountants.sql

--- database/accountants.sql
+++ database/accountants.sql
@@ -1,8 +1,13 @@
-CREATE DATABASE accountants;
+DROP SCHEMA IF EXISTS accountants CASCADE;
+CREATE SCHEMA accountants;
 
-CREATE TABLE t_acc_users (
+CREATE TABLE accountants.t_acc_users (
     id uuid PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT NOT NULL,
     password TEXT NOT NULL
 );
+
+INSERT INTO accountants.t_acc_users (id, name, email, password) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Romina', 'romy__95@gmail.com', 'puchito');
+INSERT INTO accountants.t_acc_users (id, name, email, password) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'Tuca', 'tuca__96@gmail.com', 'tucatuca');
+INSERT INTO accountants.t_acc_users (id, name, email, password) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'Yani', 'yani__96@gmail.com', 'yani123');
